d22daded5b2ccffbf50aca5e61455fafcf192a17
SamuelS <samuel_sjodin@telia.com>
2012-10-23 17:06:32 +0200

    jahaja

--- test/frontend_incoming.py
+++ test/frontend_incoming.py
@@ -13,6 +13,7 @@ q = awssqs(FRONTEND_INCOMING)
 #print inxmltext
 #parsa(inxmltext)
 #sys.exit()
+reqIDStart = 33
 
 def process(inxmltext):
 	time.sleep(1)
@@ -22,23 +23,23 @@ def process(inxmltext):
 
 #test ListCells
 for num in xrange(1,10):
-	process(XML_Request(num,'ListCells', '201210230000','201210280000',num))
+	process(XML_Request(reqIDStart+num,'ListCells', '201210230000','201210280000',num))
 	#     XML_Request(ID,RequestType,TimeStart,TimeStop,CellID):
 
 #test CellStatSpeed   #will probably produce some XML_CellIDError's
 for num in xrange(1,10):
-	process(XML_Request(10+num,'CellStatSpeed', '201210230000','201210280000',num))
+	process(XML_Request(reqIDStart+10+num,'CellStatSpeed', '201210230000','201210280000',num))
 	#     XML_Request(ID,RequestType,TimeStart,TimeStop,CellID):
 
 #test CellStatNet
 for num in xrange(1,10):
-	process(XML_Request(20+num,'CellStatNet', '201210230000','201210280000',num))
+	process(XML_Request(reqIDStart+20+num,'CellStatNet', '201210230000','201210280000',num))
 	#     XML_Request(ID,RequestType,TimeStart,TimeStop,CellID):
 
 #test error messages
-process(XML_Request(31,'CellStatNet', '201210230000','201210280000',num)) #XML error (actually fault with req-Type)
-process(XML_Request(32,'CellStatNet', '20121023000','201210280000',num)) #XML_StartTimeError
-process(XML_Request(33,'CellStatNet', '201210230000','20121028000',num)) #XML_StopTimeError
+process(XML_Request(reqIDStart+31,'CellStatNet', '201210230000','201210280000',num)) #XML error (actually fault with req-Type)
+process(XML_Request(reqIDStart+32,'CellStatNet', '20121023000','201210280000',num)) #XML_StartTimeError
+process(XML_Request(reqIDStart+33,'CellStatNet', '201210230000','20121028000',num)) #XML_StopTimeError
 
 
 print "Finish puting in testfiles"
