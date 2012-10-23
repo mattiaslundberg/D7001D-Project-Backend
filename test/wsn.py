34ed8e03f186f9486a462f129c73f5279cdb449c
Mattias Lundberg <lundberg.mattias@gmail.com>
2012-10-23 16:22:11 +0200

    testing updates

--- test/wsn.py
+++ test/wsn.py
@@ -19,16 +19,19 @@ Rawdata			xx
 if __name__ == '__main__':
 	max_tests = 10000
 	while True:
-		for f in commands.getoutput("ls ../pkt").split('\n'):
-			s = socket.socket(
-				socket.AF_INET, socket.SOCK_STREAM)
-			s.connect(('wsn.d7001d.mlundberg.se', 12345))
-			f_open = open('../pkt/%s' % f)
-			data = f_open.read()
-			f_open.close()
-			tosend = struct.pack('!IIBQI',1,2,0,time.time()*1000,len(data))
-			s.send(tosend + data)
-			s.close()
-			if max_tests < 0:
-				break
-			max_tests-=1
+		for ce in xrange(1,9):
+			for si in xrange(0,1):
+				for f in commands.getoutput("ls ../pkt").split('\n'):
+					print max_tests
+					s = socket.socket(
+						socket.AF_INET, socket.SOCK_STREAM)
+					s.connect(('wsn.d7001d.mlundberg.se', 12345))
+					f_open = open('../pkt/%s' % f)
+					data = f_open.read()
+					f_open.close()
+					tosend = struct.pack('!IIBQI',ce,2,si,time.time()*1000,len(data))
+					s.send(tosend + data)
+					s.close()
+					if max_tests < 0:
+						break
+					max_tests-=1
