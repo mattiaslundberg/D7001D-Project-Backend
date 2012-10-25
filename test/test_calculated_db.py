901684b7e290ac5cb5fc0ccfb5d6c36dec9c7dbf
Viktor Lindgren <vkg.lindgren+github@gmail.com>
2012-10-25 08:51:46 +0200

    More test commit(for testing updating process on worker)

--- test/test_calculated_db.py
+++ test/test_calculated_db.py
@@ -7,7 +7,7 @@ db = _db()
 msg = "loooooooooooooooooooooooooooooooooooooooong teeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeexxxxxxxxxxxxxxxxxxxxxxxttttttttt text"
 myid = 2
 db.write(myid,msg)
-#time.sleep(120)
+time.sleep(60)
 loaded_msg = db.read(myid)
 assert loaded_msg == msg, "put = %s and got = %s" % (msg, loaded_msg)
 print "test ok"
